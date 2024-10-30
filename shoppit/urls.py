3e3f34319df604c8a99cbdd23a28034014ec6eb5
Firomsa Hika <firomsahika2022@gmail.com>
2024-10-30 09:01:35 +0300

    added chapa integration

--- shoppit/urls.py
+++ shoppit/urls.py
@@ -12,8 +12,9 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("shop_app.urls")),
-      path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('chapa-webhook', include("chapa.urls"))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
