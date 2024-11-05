3266dc1b3a9558e5907340cb590e007e5067309d
Firomsa Hika <firomsahika2022@gmail.com>
2024-11-05 13:05:34 +0300

    added chrome driver

--- shop_app/services/scraper.py
+++ shop_app/services/scraper.py
@@ -1,24 +1,31 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 import time
+
+
+service = Service(executable_path="chromedriver.exe")
+
+
 
 def scrape_product_data(url):
     chrome_options = Options()
-    chrome_options.add_argument("--headless")  # Run Chrome in headless mode
-    chrome_options.add_argument("--no-sandbox")  # Required in some environments
+    chrome_options.add_argument("--headless")  
+    chrome_options.add_argument("--no-sandbox")  
     chrome_options.add_argument("--disable-dev-shm-usage")
-    driver = webdriver.Chrome(options=chrome_options) 
-    driver.get(url)
-    time.sleep(2) 
+    
 
     products = []
     try:
+        driver = webdriver.Chrome(service=service) 
+        driver.get(url)
+        time.sleep(3) 
         product_elements = driver.find_elements(By.ID, 'product-item')
         for product in product_elements:
             name = driver.find_element(By.ID, f'product-name-{product.id}').text
             price = driver.find_element(By.ID, f'product-price-{product.id}').text
-            ram = driver.find_element(By.ID, f'product-ram-{product.id}').textt
+            ram = driver.find_element(By.ID, f'product-ram-{product.id}').text
 
             products.append({
                 'name': name,
